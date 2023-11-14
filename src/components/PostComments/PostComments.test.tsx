3aa11f7b40f25f3944b2aacab373d4458256ee6d
alexandrecabraldev <alexandre_c.m@hotmail.com>
2023-11-14 17:56:11 -0300

    finished test 2

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-// import Post from '.';
+import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
